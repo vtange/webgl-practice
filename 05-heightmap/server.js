58861d572dfee0f9688004879348e1afab686cce
Victor Tang <victortang1992@gmail.com>
2017-02-19 11:30:09 -0800

    remove pep min

--- 05-heightmap/server.js
+++ 05-heightmap/server.js
@@ -10,6 +10,6 @@ app.get('/favicon.ico', function(req, res) {
     res.sendStatus(204);
 });
 
-app.listen(4001,function(){
+app.listen(4000,function(){
     console.log('The magic happens at port 4000.');
 });
