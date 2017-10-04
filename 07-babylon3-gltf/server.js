9334d6d235f8cec1019e7922180b910275f61aa4
Victor Tang <victortang1992@gmail.com>
2017-10-03 22:22:11 -0700

    07-babylon3 and gltf

---
+++ 07-babylon3-gltf/server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+//use static middleware in express to load static page directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/favicon.ico', function(req, res) {
+    res.sendStatus(204);
+});
+
+app.listen(4000,function(){
+    console.log('The magic happens at port 4000.');
+});
